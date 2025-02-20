299968b201f70df5f27c2f9285fac9938762f2c4
Jacob Henderson <jacobhenderson9127@gmail.com>
2025-02-20 13:06:05 -0600

    new cheddar animations

--- scripts/cheddar.gd
+++ scripts/cheddar.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -200.0
+const SPEED = 75
+const JUMP_VELOCITY = -100
 
 
 func _physics_process(delta: float) -> void:
@@ -13,6 +13,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
 		if visible == true:
 			$Jump.play()
 
