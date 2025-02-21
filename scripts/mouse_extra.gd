73df4ad363ea4f79db11b4eb52df1460223f4997
Jacob Henderson <jacobhenderson9127@gmail.com>
2025-02-21 13:02:27 -0600

    wip intro cutscene

--- scripts/mouse_extra.gd
+++ scripts/mouse_extra.gd
@@ -1,15 +1,18 @@
 extends AnimatedSprite2D
 
 @export var SPEED = 75
+@export var animation_name = ''
+@export var title_screen = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#var colors = ["964B00", "D3D3D3"]
 	#modulate = Color(colors[randi_range(0,1)])
-	play("run")
+	play(animation_name)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += SPEED * delta
-	if position.x >= get_viewport_rect().size.x + 30:
-		queue_free()
+	if title_screen:
+		position.x += SPEED * delta
+		if position.x >= get_viewport_rect().size.x + 30:
+			queue_free()
