03cdfe8c972a72b4ca1d5e6cfdeab33ee226d72f
Jacob Henderson <jacobhenderson9127@gmail.com>
2025-02-28 11:47:33 -0600

    added camera pan @ beginning

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var camera_pan
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$map.hide()
@@ -10,11 +10,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	## Code for initial camera pan to oven.
+	while camera_pan:
+		await get_tree().create_timer(0.05).timeout
+		$Cheddar/Camera2D.global_position.x -= 0.1
+		if $Cheddar/Camera2D.global_position.x <= 750:
+			camera_pan = false
+			await get_tree().create_timer(2.0).timeout
+			$Cheddar/Camera2D.global_position.x = 1000
+			$Cheddar.show()
 
 
 func _on_game_start() -> void:
 	$StartScreen.hide()
 	$map.show()
-	$Cheddar.show()
+	$Cheddar/Camera2D.zoom = Vector2(4,4)
 	$Cheddar/Camera2D.enabled = true
+	camera_pan = true
