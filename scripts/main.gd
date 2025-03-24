0d470a8858b3de19cb5d1b2a01bf2640047db755
Jacob Henderson <jacobhenderson9127@gmail.com>
2025-03-24 15:43:23 -0500

    clean up on aisle main

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 var camera_pan
 var oven_minigame = preload("res://scenes/oven.tscn")
 var running_minigame = preload("res://scenes/runningMiniGame.tscn")
+var safe_minigame = preload("res://scenes/safe_minigame.tscn")
 var completed_oven = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,65 +13,14 @@ func _ready() -> void:
 	$Cheddar/Camera2D.enabled = false
 	$Cheddar.can_move = false
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	## Handling HUD movement & limits.
-	if $Cheddar.position.x <= 760 and $Cheddar.position.x >= 66:
-		$HUD.position.x = $Cheddar.position.x
-	elif $Cheddar.position.x > 760:
-		$HUD.position.x = 759
-	elif $Cheddar.position.x < 66:
-		$HUD.position.x = 60
-	## Code for initial camera pan to oven.
-	while camera_pan:
-		$Directions.show()
-		await get_tree().create_timer(0.1).timeout
-		$Cheddar/Camera2D.global_position.x -= 0.05
-		if $Cheddar/Camera2D.global_position.x <= 750:
-			camera_pan = false
-			await get_tree().create_timer(2.0).timeout
-			$Cheddar/Camera2D.global_position.x = 1000
-			$Cheddar.show()
-			$Cheddar.can_move = true
-			$HUD.show()
-			$Directions.hide()
-			
-			
+	handle_HUD()
 	if Input.is_action_just_pressed("pick_up_item"):
 		if abs($Cheddar.position.x - 630) <= 50 and not completed_oven and $Cheddar.items_remaining <= 0:
-			## Oven minigame setup.
-			var oven = oven_minigame.instantiate()
-			$map.hide()
-			$StaticBody2D.hide()
-			$HUD.hide()
-			$StaticBody2D/CollisionShape2D.disabled = true
-			$Cheddar.position = Vector2(613, 410)
-			$Cheddar/Camera2D.zoom = Vector2(3,3)
-			$Cheddar/Camera2D.limit_left = 510
-			$Cheddar/Camera2D.limit_bottom = 550
-			$Cheddar/Camera2D.limit_right = 930
-			oven.get_node("OvenHUD").connect("won", _on_oven_minigame_win)
-			play_sfx("minigame")
-			add_child(oven)
-			
+			oven_minigame_setup()
 		elif abs($Cheddar.position.x - 910) <= 50 and completed_oven:
-			## Running minigame setup.
-			var running = running_minigame.instantiate()
-			$PostOvenText.hide()
-			$Cheddar.position = Vector2(300, 410)
-			$Cheddar.scale = Vector2(3,3)
-			$Cheddar/Camera2D.enabled = false
-			$Cheddar.JUMP_VELOCITY = -500.0
-			$Cheddar.running_minigame = true
-			$map.hide()
-			$StaticBody2D.hide()
-			$HUD.hide()
-			$StaticBody2D/CollisionShape2D.disabled = true
-			for fire in get_tree().get_nodes_in_group("main_flames"):
-				fire.hide()
-			add_child(running)
-
+			chase_minigame_setup()
 
 func _on_game_start() -> void:
 	$StartScreen.hide()
@@ -78,9 +28,27 @@ func _on_game_start() -> void:
 	$Cheddar/Camera2D.zoom = Vector2(4,4)
 	$Cheddar/Camera2D.global_position = Vector2(900, 370)
 	$Cheddar/Camera2D.enabled = true
-	play_sfx('main')
+	$Directions.show()
 	camera_pan = true
+	play_sfx('main')
+	pan_to_location(750)
 	
+func oven_minigame_setup():
+	## Oven minigame setup.
+	var oven = oven_minigame.instantiate()
+	$map.hide()
+	$StaticBody2D.hide()
+	$HUD.hide()
+	$StaticBody2D/CollisionShape2D.disabled = true
+	$Cheddar.position = Vector2(613, 410)
+	$Cheddar/Camera2D.zoom = Vector2(3,3)
+	$Cheddar/Camera2D.limit_left = 510
+	$Cheddar/Camera2D.limit_bottom = 550
+	$Cheddar/Camera2D.limit_right = 930
+	oven.get_node("OvenHUD").connect("won", _on_oven_minigame_win)
+	play_sfx("minigame")
+	add_child(oven)
+
 func _on_oven_minigame_win():
 	completed_oven = true
 	$map.show()
@@ -95,6 +63,23 @@ func _on_oven_minigame_win():
 	$PostOvenText.show()
 	for fire in get_tree().get_nodes_in_group("main_flames"):
 		fire.show()
+
+func chase_minigame_setup():
+	## Running minigame setup.
+	var running = running_minigame.instantiate()
+	$PostOvenText.hide()
+	$Cheddar.position = Vector2(300, 410)
+	$Cheddar.scale = Vector2(3,3)
+	$Cheddar/Camera2D.enabled = false
+	$Cheddar.JUMP_VELOCITY = -500.0
+	$Cheddar.running_minigame = true
+	$map.hide()
+	$StaticBody2D.hide()
+	$HUD.hide()
+	$StaticBody2D/CollisionShape2D.disabled = true
+	for fire in get_tree().get_nodes_in_group("main_flames"):
+		fire.hide()
+	add_child(running)
 
 func play_sfx(state):
 	## Handles sound effects.
@@ -127,3 +112,26 @@ func play_sfx(state):
 	while audio_player.volume_db < desired_volume:
 		audio_player.volume_db += 2
 		await get_tree().create_timer(1.0).timeout
+		
+func handle_HUD():
+	## Handling HUD movement & limits.
+	if $Cheddar.position.x <= 760 and $Cheddar.position.x >= 66:
+		$HUD.position.x = $Cheddar.position.x
+	elif $Cheddar.position.x > 760:
+		$HUD.position.x = 759
+	elif $Cheddar.position.x < 66:
+		$HUD.position.x = 60
+	
+func pan_to_location(loc, start=1000):
+	## Code for initial camera pan to oven.
+	while camera_pan:
+		await get_tree().create_timer(0.01).timeout
+		$Cheddar/Camera2D.global_position.x -= 1
+		if $Cheddar/Camera2D.global_position.x <= loc:
+			camera_pan = false
+			await get_tree().create_timer(2.0).timeout
+			$Cheddar/Camera2D.global_position.x = start
+			$Cheddar.show()
+			$Cheddar.can_move = true
+			$HUD.show()
+			$Directions.hide()
